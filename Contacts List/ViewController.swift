380f2aea597150c1b19ca5d15eae3e423bfdaac7
Marwan Alani <marwan@alani.ca>
2018-11-03 18:01:58 -0400

    Initial Commit

---
+++ Contacts List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts List
+//
+//  Created by Marwan Alani on 2018-11-03.
+//  Copyright © 2018 Marwan Alani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
