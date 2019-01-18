2ec1a616dfc671c78cb4b436b2d0242295ce930d
Marwan Alani <marwan@alani.ca>
2019-01-18 10:12:28 -0500

    Added more small touches post-workshop

---
+++ Contacts List/Source/UserDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserDetailsViewController.swift
+//  Contacts List
+//
+//  Created by Marwan Alani on 2018-11-06.
+//  Copyright © 2018 Marwan Alani. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
